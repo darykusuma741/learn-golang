931ba196c9cfa2346417b056dcd9d97d054b44f4
Dary Kusuma Hardian <darykusuma741@gmail.com>
2026-01-28 21:03:26 +0700

    hello world

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	hello_world "learn-golang/pkg"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	hello_world.SayHello()
 }
---
+++ pkg/hello_world.go
@@ -0,0 +1,7 @@
+package hello_world
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello World")
+}
